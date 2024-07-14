af2f583ea0d87c65e09b3389c69691a0556771b1
Takayuki Hira <takayuki@M3-MacBook-Air.local>
2024-07-15 02:43:51 +0900

    add spring boot application

---
+++ app/src/main/java/com/munetake3/rechargeable_battery_management/RechargeableBatteryManagementApplication.java
@@ -0,0 +1,13 @@
+package com.munetake3.rechargeable_battery_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RechargeableBatteryManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RechargeableBatteryManagementApplication.class, args);
+	}
+
+}
---
+++ app/src/test/java/com/munetake3/rechargeable_battery_management/RechargeableBatteryManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.munetake3.rechargeable_battery_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RechargeableBatteryManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
